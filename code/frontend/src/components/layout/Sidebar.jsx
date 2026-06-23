f2d4b451404eff24b202c5f0357ba923d1667074
Phạm Duy Hưng <phamduyhung0809@gmail.com>
2026-06-23 14:16:48 +0700

    fix: resolve merge conflicts, keep teal UI - remove search from FloatingTopBar

--- code/frontend/src/components/layout/Sidebar.jsx
+++ code/frontend/src/components/layout/Sidebar.jsx
@@ -3,7 +3,6 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
 import useAuthStore from '@store/useAuthStore'
 import useProjectStore from '@store/useProjectStore'
-import useLayoutStore from '@store/useLayoutStore'
 import toast from 'react-hot-toast'
 import { getInitials } from '@utils/avatarHelper'
 
